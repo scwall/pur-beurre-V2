d121987f2150c25dc94bf99c99ab9576ca876e19
Administrator <deselys.pascal@gmail.com>
2018-07-05 23:06:45 +0200

    Change templates, add img, change url, change test function

--- food_and_search/tests.py
+++ food_and_search/tests.py
@@ -4,13 +4,26 @@ from django.test import TestCase
 from food_and_search.models import Product, Categorie
 
 
-class AnimalTestCase(TestCase):
+class ProductTestCase(TestCase):
     def setUp(self):
-        Categorie.objects.create(name='fruit')
-        Product.objects.create(name="pomme", description="une pomme")
+        Categorie.objects.create(name='fruit',id_category='fruit')
+        Product.objects.create(name="pomme",
+                               description="une pomme",
+                               nutrition_grade='a',
+                               saturated_fat_100g='9.9',
+                               carbohydrates_100g='9.9',
+                               energy_100g='9.9',
+                               sugars_100g='9.9',
+                               sodium_100g='9.9')
 
 
-    def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
+    def test_product(self):
         product = Product.objects.get(name="pomme")
         self.assertEqual(product.description , 'une pomme')
+        self.assertEqual(product.nutrition_grade , 'a')
+        self.assertEqual(product.saturated_fat_100g, 9.9)
+        self.assertEqual(product.carbohydrates_100g, 9.9)
+        self.assertEqual(product.energy_100g, 9.9)
+        self.assertEqual(product.sugars_100g,9.9)
+        self.assertEqual(product.sodium_100g,9.9)
+
