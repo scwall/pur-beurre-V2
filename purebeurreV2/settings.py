0f07ec15f5e7302059c98bf2904db7a821f55885
Administrator <deselys.pascal@gmail.com>
2018-07-18 13:02:54 +0200

    modify road static file

--- purebeurreV2/settings.py
+++ purebeurreV2/settings.py
@@ -131,7 +131,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
+        os.path.join(PROJECT_ROOT,'food_and_search', 'static'),
     )
 if os.environ.get('ENV') == 'PRODUCTION':
     # ...
