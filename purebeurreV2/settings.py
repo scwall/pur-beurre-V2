118cc55d5cc8131e729317f5fb5b10ab12f74c6c
Administrator <deselys.pascal@gmail.com>
2018-08-01 11:08:19 +0200

    modify generator secret key

--- purebeurreV2/settings.py
+++ purebeurreV2/settings.py
@@ -30,12 +30,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.environ.get('SECRET_KEY', '4i&u(!%shd*0-3$ls)fohsjsd48t(gu%1-ch_wyzk7@#n3bd8e')
 if os.environ.get('ENV') == 'PRODUCTION':
     DEBUG = False
     #SECRET_KEY = "".join([random.choice(string.printable) for _ in range(24)])
 else:
     DEBUG = True
-    SECRET_KEY = 'gjtd%4j^9pq1y108nwl!5ku#h4sd9-%xzi0ibhnn7@pyh5nkwk'
 
 if os.environ.get('ENV') == 'PRODUCTION':
     ALLOWED_HOSTS = ['pur-beurre-scwalito.herokuapp.com']
